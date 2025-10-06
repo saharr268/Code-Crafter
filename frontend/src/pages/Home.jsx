5a1fa71ac4ad64b1d308bd3fe5b47a1cebd52aad
HakimaNoori <hakima.noori.1379@gmail.com>
2025-10-06 21:16:24 +0430

    fix(main): changes code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import MobileBanner from "../components/MobileBanner";
-import PopularTopics from "../components/PopularTopics";
+import MobileBanner from "../components/home/MobileBanner";
+import PopularTopics from "../components/home/PopularTopics";
 import Footer from "../components/common/Footer";
 import AboutSection from "../components/home/AboutSection";
 import BannerSection from "../components/home/BannerSection";
 import CardSection from "../components/home/CardSection";
-import CommentsSection from "../components/home/CommentsSection";
-import MostViewedSection from "../components/home/MostViewedSection";
 import HeroSection from "../components/home/HeroSection";
 import HeroBanner from "../components/home/HeroBanner";
 
 const Home = () => {
   return (
     <div>
-      <MobileBanner/>
-       <PopularTopics />
       <HeroSection />
       <HeroBanner />
-      <AboutSection/>
+      <AboutSection />
       <CardSection />
-      <CommentsSection />
       <BannerSection />
-      <MostViewedSection />
+      <MobileBanner />
+      <PopularTopics />
       <Footer />
     </div>
   );
