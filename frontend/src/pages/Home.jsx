2a35c4c44c4689bd31420163629dd7e32ea5eeea
Marzia Hashimi <marziajan130@gmail.com>
2025-10-07 22:55:30 +0500

    Add ResourcesPage layout with styled cards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MobileBanner from "../components/MobileBanner";
 import PopularTopics from "../components/PopularTopics";
+import ResourcesPage from "../components/ResourcesPage";
 
 const Home = () => {
   return (
     <div>
       <MobileBanner/>
        <PopularTopics />
+        <ResourcesPage />
     </div>
   );
 };
