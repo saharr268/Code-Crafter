54287a3c42a097e7f37085903b5d99ad89d4e0d0
HakimaNoori <hakima.noori.1379@gmail.com>
2025-10-07 11:08:52 +0430

    fix(main): changes code at the messageBox  sectionand  support page

--- frontend/src/pages/SupportPage.jsx
+++ frontend/src/pages/SupportPage.jsx
@@ -1,14 +1,15 @@
 import Faq from '../helpers/Faq'
 import Footer from '../components/common/Footer'
 import OfflineTips from '../helpers/OfflineTips'
-import ChatBox from "../helpers/ChatBox";
 import AnswerQuestions from '../helpers/AnswerQuestions'
+import MessageBox from '../helpers/MessageBox'
 
 const SupportPage = () => {
   return (
     <div>
       <Faq />
       <OfflineTips />
+      <MessageBox/>
       <AnswerQuestions/>
       <Footer/>
     </div>
