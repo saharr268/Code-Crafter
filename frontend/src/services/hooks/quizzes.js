04a3395edc229503715063e6b67dc62644819a71
moonlight <mahtabsalarii04@gmail.com>
2025-10-15 15:19:02 +0330

    📦 NEW: Question and Quiz

---
+++ frontend/src/services/hooks/quizzes.js
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getQuizById, getQuizData } from "../configs/apiEndPoint";
+
+export const useQuizData = (pageNumber, pageSize, keyword) => {
+  return useQuery({
+    queryKey: ["quiz-data", pageNumber, pageSize, keyword],
+    queryFn: () =>
+      getQuizData({
+        pageNumber,
+        pageSize,
+        keyword,
+      }),
+  });
+};
+
+// Get quiz by id
+export const useGetQuizById = (id) => {
+  return useQuery({
+    queryKey: ["quiz-get-by-id", id],
+    queryFn: async () => (id ? await getQuizById(id) : {}),
+    enabled: !!id,
+  });
+};
