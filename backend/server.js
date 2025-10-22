5dbf62a4eb8ec739fcb6943d14ae05adb9530998
moonlight <mahtabsalarii04@gmail.com>
2025-10-22 18:57:58 +0330

    🐛 FIX: server

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/uploads", e.static(path.join(__dirname, "uploads")));
 app.use(e.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://code-crafters-seda.onrender.com",
   })
 );
 
