4025839143a5df82e3b7c199a3254f507368b3a5
HakimaNoori <hakima.noori.1379@gmail.com>
2025-10-22 06:40:53 +0430

    add responsive faq section at the help page

--- frontend/src/components/support/Help.jsx
+++ frontend/src/components/support/Help.jsx
@@ -1,7 +1,5 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
-import { useState } from "react";
-import { FaRegImage } from "react-icons/fa";
 import { useFormik } from "formik";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
@@ -13,29 +11,29 @@ import { PAGE_SIZE } from "../../helpers/constant/statics";
 import { map } from "lodash";
 import { CustomShimmer } from "../controllers/CustomShimmer";
 import { CustomRate } from "../controllers/CustomRate";
-// eslint-disable-next-line no-unused-vars
 import { motion, AnimatePresence } from "framer-motion";
 
 const Help = () => {
   const faqRef = useRef(null);
   const tipsRef = useRef(null);
   const messageRef = useRef(null);
-  const answersRef = useRef(null);
+  const [openIndex, setOpenIndex] = useState(null);
+  const [showMenu, setShowMenu] = useState(false);
 
   const scrollToSection = (ref) => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  const [openIndex, setOpenIndex] = useState(null);
   const toggleFAQ = (index) => setOpenIndex(openIndex === index ? null : index);
+
   const { data: faqsSearch, isPending: isLoadingFaq } = useFaqData(
     1,
     PAGE_SIZE,
     undefined
   );
 
-  const { mutateAsync: createComment, isPending: isLoading } =
-    useCreateComment();
+  const { mutateAsync: createComment, isPending: isLoading } = useCreateComment();
+
   const formIK = useFormik({
     initialValues: { comment_text: "", rate: "", is_accepted: "" },
     enableReinitialize: true,
@@ -64,12 +62,13 @@ const Help = () => {
     <div className="relative bg-white text-right">
       <Navbar className={"top-6"} />
       {/* Main Content Container */}
-      <div dir="rtl" className="pt-32">
-        {/* Main Content */}
-        <div className="max-w-7xl w-full grid grid-cols-1 lg:grid-cols-3">
+      <div
+        dir="rtl"
+        className="sm:mt-10 lg:mt-0 pt-24 sm:pt-32 lg:pt-32 px-4 sm:px-6 lg:px-0"
+      >
+        <div className="max-w-7xl w-full grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Sticky Sidebar */}
-          <aside className="sticky top-32 right-0  bg-black text-white rounded-2xl p-10 h-fit w-[300px] z-30 float-right mr-8">
-            {/* <h3 className="text-teal-400 mb-4">پشتیبانی صدا</h3> */}
+          <aside className="hidden lg:block sticky top-32 right-0 bg-black text-white rounded-2xl p-10 h-fit md:w-[300px] z-30 mr-8">
             <ul className="space-y-8 text-m">
               <li
                 onClick={() => scrollToSection(faqRef)}
@@ -89,46 +88,89 @@ const Help = () => {
               >
                 صندوق ارسال پیام
               </li>
-              <li
-                onClick={() => scrollToSection(answersRef)}
-                className="cursor-pointer hover:text-teal-400"
-              >
-                پاسخ پرسش‌های شما
-              </li>
             </ul>
           </aside>
-
-          <main className="w-full pl-12 lg:col-span-2 order-2 ml-12 lg:order-2">
-            {/* --- FAQ Section --- */}
+          {/* Floating Button - Mobile */}
+          <div className="fixed bottom-6 right-6 lg:hidden z-50">
+            <button
+              onClick={() => setShowMenu((prev) => !prev)}
+              className="w-[60px] h-[60px] flex justify-center items-center bg-black text-white rounded-2xl shadow-lg transition-all duration-300"
+            >
+              <span className="text-2xl font-bold">•••</span>
+            </button>
+            <AnimatePresence>
+              {showMenu && (
+                <motion.div
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 10 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute bottom-16 right-0 bg-black text-white rounded-2xl shadow-lg p-4 w-40 space-y-3"
+                >
+                  <p
+                    onClick={() => {
+                      scrollToSection(faqRef);
+                      setShowMenu(false);
+                    }}
+                    className="cursor-pointer hover:text-teal-400"
+                  >
+                    سوالات مکرر
+                  </p>
+                  <p
+                    onClick={() => {
+                      scrollToSection(tipsRef);
+                      setShowMenu(false);
+                    }}
+                    className="cursor-pointer hover:text-teal-400"
+                  >
+                    نکات آفلاین
+                  </p>
+                  <p
+                    onClick={() => {
+                      scrollToSection(messageRef);
+                      setShowMenu(false);
+                    }}
+                    className="cursor-pointer hover:text-teal-400"
+                  >
+                    پیام
+                  </p>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
+          {/* Main Section */}
+          <main className="w-full px-2 sm:px-6 lg:pl-12 lg:col-span-2 order-2 lg:order-2">
+            {/* FAQ Section */}
             <section ref={faqRef} className="bg-white pb-20">
-              <div className="max-w-full px-8">
+              <div className="max-w-full px-4 sm:px-8">
                 <h2 className="text-2xl font-bold mb-8">سوالات مکرر</h2>
                 <div className="space-y-4">
                   {isLoadingFaq
-                    ? map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], () => (
-                        <div className="w-full h-[40px] bg-gray-100 rounded-lg animate-pulse" />
+                    ? map([1, 2, 3, 4, 5, 6, 7], () => (
+                        <CustomShimmer
+                          key={`shimmer-${Math.random()}`}
+                          className="w-full h-[40px] bg-gray-100 rounded-lg animate-pulse"
+                        />
                       ))
-                    : faqsSearch?.data?.map(({ title, descriptoin }, index) => (
+                    : faqsSearch?.data?.map(({ title, description }, index) => (
                         <div
-                          dir="ltr"
                           key={index}
+                          dir="rtl" // تنظیم جهت RTL برای هر آیتم FAQ
                           className="border rounded-lg shadow-sm bg-background-card transition overflow-hidden"
                         >
                           <button
                             onClick={() => toggleFAQ(index)}
                             className="w-full flex items-center justify-between px-4 py-3"
                           >
+                            <span className="text-gray-700 font-medium">
+                              {title}
+                            </span>
                             <FiChevronDown
                               className={`text-gray-500 transition-transform duration-300 ${
                                 openIndex === index ? "rotate-180" : ""
                               }`}
                             />
-                            <span className="text-gray-700 font-medium">
-                              {title}
-                            </span>
                           </button>
-
-                          {/* Animated Section */}
                           <AnimatePresence initial={false}>
                             {openIndex === index && (
                               <motion.div
@@ -149,9 +191,9 @@ const Help = () => {
                                   animate={{ y: 0 }}
                                   exit={{ y: -5 }}
                                   transition={{ duration: 0.25 }}
-                                  className="px-4 pb-4 text-gray-600 text-sm leading-relaxed"
+                                  className="px-4 pb-4 text-gray-600 text-sm leading-relaxed text-right"
                                 >
-                                  {descriptoin}
+                                  {description}
                                 </motion.div>
                               </motion.div>
                             )}
@@ -161,8 +203,7 @@ const Help = () => {
                 </div>
               </div>
             </section>
-
-            {/* --- Offline Tips Section --- */}
+            {/* Offline Tips Section */}
             <section
               dir="rtl"
               ref={tipsRef}
@@ -171,59 +212,35 @@ const Help = () => {
               <div className="max-w-5xl mx-auto leading-8">
                 <h2 className="text-2xl mb-6">نکات دسترسی آفلاین</h2>
                 <p>
-                  تقریباً تمامی مرورگرها در پلتفرم‌های مختلف، گزینه‌ای برای
-                  دانلود کامل صفحات وب در اختیار کاربران می‌گذارند. این گزینه در
-                  مرورگرهایی مانند کروم و فایرفاکس تحت عنوان Save webpage as در
-                  دسترس است و امکان دانلود یک صفحه‌ی وب کامل شامل تصاویر و
-                  قالب‌بندی آن را فراهم می‌کند که بعدا می‌توان آن‌ها را در
-                  مرورگر باز و مطالعه کرد. در دستگاه‌های اپل، می‌توان از گزینه‌ی
-                  Reading List مرورگر سافاری برای ذخیره و استفاده‌ی آفلاین صفحات
-                  وب استفاده کرد.
+                  تقریباً تمامی مرورگرها در پلتفرم‌های مختلف، گزینه‌ای برای دانلود کامل صفحات وب در اختیار کاربران می‌گذارند...
                 </p>
                 <p className="mb-6">
-                  تقریبا تمامی مرورگرها در پلتفرم‌های مختلف گزینه‌ای برای دانلود
-                  کامل صفحات وب دارند. در مرورگرهای مختلف با عناوینی مانند{" "}
-                  <span className="font-semibold">Save webpage</span> یا{" "}
-                  <span className="font-semibold">Save page</span> شناخته
-                  می‌شوند و امکان مرور آفلاین را فراهم می‌کنند.
+                  تقریبا تمامی مرورگرها در پلتفرم‌های مختلف گزینه‌ای برای دانلود کامل صفحات وب دارند...
                 </p>
-                <h3 className="font-semibold text-lg mb-2">
-                  ذخیره وب سایت در Google Chrome
-                </h3>
+                <h3 className="font-semibold text-lg mb-2">ذخیره وب سایت در Google Chrome</h3>
                 <ol className="list-decimal list-inside space-y-1 mb-6">
-                  <li>در گوشه بالا سمت راست، منوی مرورگر را باز کنید.</li>
-                  <li>روی گزینه Save page as کلیک کنید.</li>
-                  <li>می‌توانید Cast، Save و Share را انتخاب کنید.</li>
+                  <li>در گوشه بالا سمت راست، منوی مرورگر را باز کنید。</li>
+                  <li>روی گزینه Save page as کلیک کنید。</li>
+                  <li>می‌توانید Cast، Save و Share را انتخاب کنید。</li>
                 </ol>
                 <p className="mt-8">
-                  همچنین می‌توانید با کلیدهای{" "}
-                  <span className="font-semibold">Ctrl + S</span> در ویندوز یا{" "}
-                  <span className="font-semibold">Command + S</span> در مک نیز
-                  این کار را انجام دهید.
+                  همچنین می‌توانید با کلیدهای <span className="font-semibold">Ctrl + S</span> در ویندوز یا <span className="font-semibold">Command + S</span> در مک نیز این کار را انجام دهید.
                 </p>
                 <p>
-                  نکته: می‌توانید به‌جای مراحل گفته شده، برای دسترسی به پنجره‌ی
-                  ذخیره‌سازی صفحه‌وب، در ویندوز از Ctrl + S و در مک از Command +
-                  S استفاده کنید. در پنجره‌ی باز شده، مطمئن شوید که Save as type
-                  روی Webpage,Complete تنظیم شده است.
+                  نکته: می‌توانید به‌جای مراحل گفته شده، برای دسترسی به پنجره‌ی ذخیره‌سازی صفحه‌وب، در ویندوز از Ctrl + S و در مک از Command + S استفاده کنید. در پنجره‌ی باز شده، مطمئن شوید که Save as type روی Webpage,Complete تنظیم شده است.
                 </p>
               </div>
             </section>
-
-            {/* --- Message Box Section --- */}
+            {/* Message Box Section */}
             <section
               ref={messageRef}
               className="w-full flex justify-center items-center py-20 px-4 bg-white"
             >
               <div dir="rtl" className="w-full max-w-3xl">
-                <h2 className="text-xl font-bold mb-3 text-gray-900">
-                  صندوق ارسال دیدگاه
-                </h2>
+                <h2 className="text-xl font-bold mb-3 text-gray-900">صندوق ارسال دیدگاه</h2>
                 <p className="text-gray-600 text-sm leading-6 mb-6">
-                  دوست عزیز! با تشکر از اینکه ما را برای همراهی انتخاب کردید؛
-                  لطفا تجربه‌ی خود را درباره‌ی وبسایت صدا با به اشتراک بگذارید.
+                  دوست عزیز! با تشکر از اینکه ما را برای همراهی انتخاب کردید؛ لطفا تجربه‌ی خود را درباره‌ی وبسایت صدا با به اشتراک بگذارید.
                 </p>
-
                 <form onSubmit={formIK.handleSubmit} className="space-y-2">
                   <div className="relative bg-background-card py-8 rounded-3xl">
                     <textarea
@@ -231,14 +248,11 @@ const Help = () => {
                       onChange={formIK.handleChange}
                       name="comment_text"
                       placeholder="پیام خود را بنویسید..."
-                      className="w-full min-h-[320px] px-8 rounded-lg bg-transparent border-gray-300 focus:ring-teal-500 focus:ring-0 focus:outline-none focus-visible:ring-0 text-gray-800 resize-none "
+                      className="w-full min-h-[320px] px-8 rounded-lg bg-transparent border-gray-300 focus:ring-teal-500 focus:ring-0 focus:outline-none focus-visible:ring-0 text-gray-800 resize-none"
                     ></textarea>
                     {formIK.errors.comment_text && (
-                      <p className="text-red-500 text-sm mt-1">
-                        {formIK.errors.comment_text}
-                      </p>
+                      <p className="text-red-500 text-sm mt-1">{formIK.errors.comment_text}</p>
                     )}
-
                     <div className="flex px-8 justify-between items-center pt-8 border-solid border-[#DDDDDD] border-t-2">
                       <CustomRate
                         onChange={formIK.handleChange}
